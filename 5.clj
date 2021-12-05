0a71d193b6a0be7fdccf4334e9eeb5f47d7814ff
Hiep Pham <hiepph.2406@gmail.com>
2021-12-06 00:25:17 +0100

    refactor(5): logic in marking diagram with coords

--- 5.clj
+++ 5.clj
@@ -3,7 +3,7 @@
 (def input
   (str/split (slurp "input/5.txt") #"\n"))
 
-(defn to-coord
+(defn to-couple-coord
   "Transform line input into [x1 y1 x2 y2]"
   [line]
   (map
@@ -11,9 +11,10 @@
    (filter (complement empty?)
            (str/split line #"[\s|\-|>|,]"))))
 
-(def coords
+;; [[x1 y1 x2 y2] [...] ...]
+(def couple-coords
   (map
-   to-coord
+   to-couple-coord
    input))
 
 ;;
@@ -21,7 +22,7 @@
 ;;
 ;; Create a 2d diagram with initial 0 values.
 ;;
-;; 1st loop: increase by 1 for each couple of corresponding coordinates
+;; 1st loop: increase by 1 for each couple of corresponding couple coordinates
 ;; provided by the input.
 ;; (only consider where x1 = x2 or y1 = y2)
 ;; Note that x1 can be larger than x2, y1 can be larger than y2
@@ -29,43 +30,55 @@
 ;; 2nd loop: count all values which are larger than 1
 ;;
 (def n
-  (inc (apply max (flatten coords))))
+  (inc (apply max (flatten couple-coords))))
 
 (def diagram
   (into [] (repeat n
                    (into [] (repeat n 0)))))
 
-(defn mark-diagram
+(defn gen-range
+  "Helper to generate a range, either from smaller number to larger number
+  or from larger number to smaller number."
+  [a b]
+  (if (< a b)
+    (range a (inc b))
+    (range a (dec b) -1)))
+
+(defn mark-diagram-with-coords
+  "Mark diagram from a list of coords [x y]"
   [diagram coord]
-  (let [[x1 y1 x2 y2] coord]
+  (reduce
+   (fn [res [x y]]
+     (update-in res [y x] inc))
+   diagram
+   coord))
+
+(defn mark-diagram
+  "Mark diagram derived from a couple of coord [x1 y1 x2 y2]"
+  [diagram couple-coord]
+  (let [[x1 y1 x2 y2] couple-coord]
     (if (= y1 y2)
       ;; horizontally
-      (let [[x-begin x-end] (sort [x1 x2])]
-        (reduce
-         (fn [res x]
-           (update-in res [y1 x] inc))
-         diagram
-         (range x-begin (inc x-end))))
+      (mark-diagram-with-coords
+       diagram
+       (map #(vector % y1) (gen-range x1 x2)))
 
       ;; vertically
-      (let [[y-begin y-end] (sort [y1 y2])]
-        (reduce
-         (fn [res y]
-           (update-in res [y x1] inc))
-         diagram
-         (range y-begin (inc y-end)))))))
+      (mark-diagram-with-coords
+       diagram
+       (map #(vector x1 %) (gen-range y1 y2))))))
 
 (def final-diagram
   (loop [diagram diagram
-         coords (filter (fn [coord]
-                          (let [[x1 y1 x2 y2] coord]
+         couple-coords (filter (fn [couple-coord]
+                          (let [[x1 y1 x2 y2] couple-coord]
                             (or (= x1 x2)
                                 (= y1 y2))))
-                        coords)]
-    (if (empty? coords)
+                        couple-coords)]
+    (if (empty? couple-coords)
       diagram
-      (recur (mark-diagram diagram (first coords))
-             (rest coords)))))
+      (recur (mark-diagram diagram (first couple-coords))
+             (rest couple-coords)))))
 
 (def sol1
   (count (filter #(> % 1)
@@ -78,32 +91,24 @@ sol1
 ;; 2
 ;; now also consider diagonal lines when marking in the 1st loop
 ;;
-(defn gen-range
-  [a b]
-  (if (< a b)
-    (range a (inc b))
-    (range a (dec b) -1)))
-
 (defn mark-diagram-2
-  [diagram coord]
-  (let [[x1 y1 x2 y2] coord]
+  [diagram couple-coord]
+  (let [[x1 y1 x2 y2] couple-coord]
     (if (or (= x1 x2)
             (= y1 y2))
-      (mark-diagram diagram coord)
+      (mark-diagram diagram couple-coord)
       ;; diagonally
-      (reduce
-       (fn [res [x y]]
-           (update-in res [y x] inc))
+      (mark-diagram-with-coords
        diagram
        (zipmap (gen-range x1 x2) (gen-range y1 y2))))))
 
 (def final-diagram-2
   (loop [diagram diagram
-         coords coords]
-    (if (empty? coords)
+         couple-coords couple-coords]
+    (if (empty? couple-coords)
       diagram
-      (recur (mark-diagram-2 diagram (first coords))
-             (rest coords)))))
+      (recur (mark-diagram-2 diagram (first couple-coords))
+             (rest couple-coords)))))
 
 (def sol2
   (count (filter #(> % 1)
