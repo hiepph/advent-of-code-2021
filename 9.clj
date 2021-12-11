4849c41fbc9aafb713f726895bec309c8188fba5
Hiep Pham <hiepph.2406@gmail.com>
2021-12-11 19:00:10 +0100

    Backbone for day 9 part 2
    Expand functionally from a coordinate

--- 9.clj
+++ 9.clj
@@ -1,7 +1,7 @@
 (require '[clojure.string :as str])
 
 (def input
-  (str/split (slurp "input/9.txt") #"\n"))
+  (str/split (slurp "test/9.txt") #"\n"))
 
 ;;
 ;; 1
@@ -38,19 +38,47 @@
     (nth (nth locations y) x)))
 
 (defn low-point?
-  "Provide a [y x] coordinate, return the low point value"
+  "Provide a [y x] coordinate, check weather if it is a low point value,
+  that is its value is smaller than all of adjacent coords"
   [coord]
   (let [adjacent-pos (adjacent-coords coord)
         adjacent-vals (map cell-value adjacent-pos)
         cur-value (cell-value coord)]
     (every? true? (map #(< cur-value %) adjacent-vals))))
 
+(def low-points
+  (filter low-point?
+          (for [y (range limy)
+                x (range limx)]
+            [y x])))
+
 (def low-values
-  (map
-   cell-value
-   (filter low-point?
-           (for [y (range limy)
-                 x (range limx)]
-             [y x]))))
+  (map cell-value low-points))
 
 (apply + (map inc low-values))
+
+
+;;
+;; 2
+;;
+(defn expand
+  "Expand the basin from a [y x] coord.
+  Returns the count of the expanded basin."
+  ([coord] (expand coord {}))
+  ([coord visited]
+   (let [adjacent-pos (adjacent-coords coord)
+         adjacent-vals (map cell-value adjacent-pos)
+
+         cur-value (cell-value coord)
+
+         adjacent-pos-vals (map vector adjacent-pos adjacent-vals)
+         adjacent-basin-coords (map first (filter
+                                           #(and (= (- (second %) cur-value) 1)
+                                                 (not (get visited %)))
+                                           adjacent-pos-vals))]
+     (if (= (count adjacent-basin-coords) 0)
+       1
+       (apply + 1 (map #(expand % (assoc
+                                   (assoc visited coord true)
+                                   % true))
+                       adjacent-basin-coords))))))
